0a3422a2d0472a43cf390b1dea6332dd4c258aa5
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-11-06 20:23:50 +0800

    THIRD COMMIT

--- prog1-assignment4-villariza.py
+++ prog1-assignment4-villariza.py
@@ -1,18 +1,18 @@
-#Added an if-else statement concerning a systematic response based on the input of a user running a program.
-print("\nYour Personal Information\n")
+#Added an additional feature in the program = Python Color Class
+print("\n\033[93m\033[1mYour Personal Information\033[0m\n")
 
 def extract_NAA():
-    nameCredential = input("Enter your NAME here. \n> ")
-    ageCredential = float(input("\nEnter your YEAR OF AGE here in numbers. \n> "))
-    addressCredential = input("\nEnter your ADDRESS here. \n> ")
+    nameCredential = input("Enter your \033[96m\033[1mNAME\033[0m here. \n> ")
+    ageCredential = float(input("\nEnter your \033[96m\033[1mYEAR OF AGE\033[0m here in \033[92m\x1B[3m\033[1mnumbers\x1B[0m. \n> "))
+    addressCredential = input("\nEnter your \033[96m\033[1mADDRESS\033[0m here. \n> ")
     return nameCredential, ageCredential, addressCredential;
 
 def exhibit(n, a, ad):
-    print(f"\nHi, my name is {n}. I am {a:,.0f} years old and I live in {ad}.\n")
+    print(f"\nHi, my name is \033[4m\033[1m{n}\033[0m. I am \033[4m\033[1m{a:,.0f}\033[0m years old and I live in \033[4m\033[1m{ad}\033[0m.\n")
 
 nameC, ageC, addressC = extract_NAA()
 
 if ageC < 0:
-    print(f"\nIt seems that your YEAR OF AGE input is not a realistic response. No Age can display a negative integer such as: {ageC:,.0f}\n")
+    print(f"\nIt seems that your \033[96m\033[1mYEAR OF AGE\033[0m input is not a realistic response. No Age can display a \033[92m\x1B[3m\033[1mnegative integer\x1B[0m such as: \033[91m\033[1m{ageC:,.0f}\033[0m\n")
 else:
     exhibit(nameC, ageC, addressC)
