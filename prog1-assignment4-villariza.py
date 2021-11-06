0573f90a9949e1a2cbc3b01f4837fba7f16e631f
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-11-06 20:18:16 +0800

    SECOND COMMIT

--- prog1-assignment4-villariza.py
+++ prog1-assignment4-villariza.py
@@ -1,4 +1,4 @@
-#Completed my basic program with absolute functionality.
+#Added an if-else statement concerning a systematic response based on the input of a user running a program.
 print("\nYour Personal Information\n")
 
 def extract_NAA():
@@ -11,4 +11,8 @@ def exhibit(n, a, ad):
     print(f"\nHi, my name is {n}. I am {a:,.0f} years old and I live in {ad}.\n")
 
 nameC, ageC, addressC = extract_NAA()
-exhibit(nameC, ageC, addressC)
+
+if ageC < 0:
+    print(f"\nIt seems that your YEAR OF AGE input is not a realistic response. No Age can display a negative integer such as: {ageC:,.0f}\n")
+else:
+    exhibit(nameC, ageC, addressC)
