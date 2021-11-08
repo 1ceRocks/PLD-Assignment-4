204fc7646e7a3b22376b438c67f10038ed9f4aea
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-11-08 16:32:21 +0800

    CODE COMPLETED: RETURNING MULTIPLE VALUES

--- prog2-assignment4-villariza.py
+++ prog2-assignment4-villariza.py
@@ -1,5 +1,5 @@
 #Rendered a welcoming greet to a user after the initialization of the program.
-print("\n\033[39m\033[1mWelcome to Villariza Foods!\033[0m \nThe Price of an\033[0m \033[91m\033[1mApple\033[0m is \033[92m\033[1m\033[1m20 PHP\033[0m. \n\033[39m\033[2mThe Price of an\033[0m \033[93m\033[1mOrange\033[0m \033[2mis\033[0m \033[92m\033[1m\033[1m25 PHP\033[0m.")
+print("\n\033[39;1mWelcome to Villariza Foods!\033[0m \nThe Price of an \033[91;1mApple\033[0m is \033[92;1m20 PHP\033[0m. \n\033[39;2mThe Price of an\033[0m \033[93;1mOrange\033[0m \033[2mis\033[0m \033[92;1m25 PHP\033[0m.")
 
 #Returning Multiple Variables (User Input in a Single-Filed Function)
 #Added an additional feature in the program = Python Color Class
@@ -7,13 +7,13 @@ def exhibit_UserPrice():
     """
     Input define function.
     """
-    apple = int(input("\nPlease \033[34m\033[1mquantify\033[0m how many \033[91m\033[1mApple\033[0m\033[1m(s)\033[0m you need to purchase... \n> \033[92m\033[1mQuantity:\033[0m\033[91m\033[1m "))
-    orange = int(input("\033[0m\nPlease \033[34m\033[1mquantify\033[0m how many \033[93m\033[1mOrange\033[0m\033[1m(s)\033[0m you need to purchase... \n> \033[92m\033[1mQuantity:\033[0m\033[93m\033[1m "))
+    apple = int(input("\nPlease \033[34;1mquantify\033[0m how many \033[91;1mApple\033[0m\033[1m(s)\033[0m you need to purchase... \n> \033[92;1mQuantity:\033[0m\033[91;1m "))
+    orange = int(input("\033[0m\nPlease \033[34;1mquantify\033[0m how many \033[93;1mOrange\033[0m\033[1m(s)\033[0m you need to purchase... \n> \033[92;1mQuantity:\033[0m\033[93;1m "))
     """
     Output string with print function.
     """
     _grand_total = obtain_totalPrice(APL = apple, ORNG = orange)
-    print(f"\033[0m\nThe total amount is \033[92m\033[2m₱ {_grand_total:,.2f}\033[0m\n")
+    print(f"\033[0m\nThe total amount is \033[92;4;1m₱ {_grand_total:,.2f}\033[0m\n")
 
 def obtain_totalPrice(APL, ORNG):
     """
