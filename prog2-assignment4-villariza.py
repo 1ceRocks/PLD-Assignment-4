6bd8e151936535ce0c48a168f2f6cfa880d600c4
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-11-10 16:21:26 +0800

    CODE COMPLETED: RETURNING MULTIPLE VALUES

--- prog2-assignment4-villariza.py
+++ prog2-assignment4-villariza.py
@@ -9,17 +9,18 @@ def exhibit_UserPrice():
     """
     apple = int(input("\nPlease \033[34;1mquantify\033[0m how many \033[91;1mApple\033[0m\033[1m(s)\033[0m you need to purchase... \n> \033[92;1mQuantity:\033[0m\033[91;1m "))
     orange = int(input("\033[0m\nPlease \033[34;1mquantify\033[0m how many \033[93;1mOrange\033[0m\033[1m(s)\033[0m you need to purchase... \n> \033[92;1mQuantity:\033[0m\033[93;1m "))
-    """
-    Output string with print function.
-    """
-    _grand_total = obtain_totalPrice(APL = apple, ORNG = orange)
-    print(f"\033[0m\nThe total amount is \033[92;4;1m₱ {_grand_total:,.2f}\033[0m\n")
+    return apple, orange;
+
+APL_input, ORNG_input = exhibit_UserPrice()
 
 def obtain_totalPrice(APL, ORNG):
     """
     Process and computation define function.
     """
-    total = (20 * APL) + (25 * ORNG)
-    return total;
+    apl_orng_total = (20 * APL) + (25 * ORNG)
+    """
+    Output string with print function.
+    """
+    print(f"\033[0m\n\033[34;1mYour Recorded Output as Tracked\033[0m\n\033[91;1mApple\033[0m Input = \033[91;1m{APL}\033[0m\n\033[93;1mOrange\033[0m Input = \033[93;1m{ORNG}\033[0m\nThe total amount would be \033[92;4;1m₱ {apl_orng_total:,.2f}\033[0m.\n")
 
-exhibit_UserPrice()
+obtain_totalPrice(APL_input, ORNG_input)
